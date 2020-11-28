487dce06f17956faad9ae98dadd99131dfcf7b26
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-28 18:45:37 -0300

    Add PrivacyController for routing privacy policy razor page

---
+++ Src/Controllers/PrivacyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntiKernelBlog.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        [Route("/privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
